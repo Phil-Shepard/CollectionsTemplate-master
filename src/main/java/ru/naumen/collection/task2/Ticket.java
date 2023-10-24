5c471f6e6e65fce85dc459cfcc82e5a88695800f
Phil-Shepard <Philipp-74@yandex.ru>
2023-10-24 21:51:48 +0500

    Сделал вторую задачу

--- src/main/java/ru/naumen/collection/task2/Ticket.java
+++ src/main/java/ru/naumen/collection/task2/Ticket.java
@@ -1,5 +1,7 @@
 package ru.naumen.collection.task2;
 
+import java.util.Objects;
+
 /**
  * Билет
  *
@@ -9,4 +11,30 @@ package ru.naumen.collection.task2;
 public class Ticket {
     private long id;
     private String client;
+
+    public Ticket(long id, String client) {
+        this.id = id;
+        this.client = client;
+    }
+
+    public long getId(){
+        return this.id;
+    }
+
+    public String getClient(){
+        return this.client;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket = (Ticket) o;
+        return id == ticket.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
