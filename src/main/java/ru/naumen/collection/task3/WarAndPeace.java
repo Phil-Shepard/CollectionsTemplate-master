7b7a37d637bf7ebdb1a928ca4197a2f96a8b4469
Phil-Shepard <Philipp-74@yandex.ru>
2023-10-26 17:57:45 +0500

    Сделал третью задачу

--- src/main/java/ru/naumen/collection/task3/WarAndPeace.java
+++ src/main/java/ru/naumen/collection/task3/WarAndPeace.java
@@ -1,6 +1,7 @@
 package ru.naumen.collection.task3;
 
 import java.nio.file.Path;
+import java.util.*;
 
 /**
  * <p>Написать консольное приложение, которое принимает на вход произвольный текстовый файл в формате txt.
@@ -18,6 +19,64 @@ public class WarAndPeace {
             "Лев_Толстой_Война_и_мир_Том_1,_2,_3,_4_(UTF-8).txt");
 
     public static void main(String[] args) {
-        // TODO
+        HashMap<String, Integer> wordToCount = new HashMap<>();
+        new WordParser(WAR_AND_PEACE_FILE_PATH)
+                .forEachWord(word -> {
+                    if (!wordToCount.containsKey(word))
+                    {
+                        wordToCount.put(word, 0);
+                    }
+                    wordToCount.put(word, wordToCount.get(word) + 1);
+        });
+
+        //Нужно собрать все встречающийся слова и посчитать для каждого из них количество раз, сколько слово встретилось
+        System.out.println("Все слова:");
+        for (String word : wordToCount.keySet())
+        {
+            System.out.println(word + " " + wordToCount.get(word));
+        }
+        System.out.println("");
+
+        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(wordToCount.entrySet());
+
+
+        Collections.sort(entryList, (entry1, entry2) -> entry1.getValue().compareTo(entry2.getValue()));
+
+        LinkedHashMap<String, Integer> sortedHashMap = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> entry : entryList) {
+            sortedHashMap.put(entry.getKey(), entry.getValue());
+        }
+
+
+        //Топ-10 самых редких слов
+        System.out.println("Топ-10 самых редких слов");
+        int count = 0;
+        for (Map.Entry<String, Integer> entry : sortedHashMap.entrySet()) {
+            if (count < 10) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+                count++;
+            } else {
+                break;
+            }
+        }
+        System.out.println("");
+
+        //Топ-10 самых частых слов
+        System.out.println("Топ-10 самых частых слов");
+        LinkedHashMap<String, Integer> reversedHashMap = new LinkedHashMap<>();
+        List<Map.Entry<String, Integer>> reversedList = new ArrayList<>(sortedHashMap.entrySet());
+        Collections.reverse(reversedList);
+        for (Map.Entry<String, Integer> entry : reversedList) {
+            reversedHashMap.put(entry.getKey(), entry.getValue());
+        }
+        int count1 = 0;
+        for (Map.Entry<String, Integer> entry : reversedHashMap.entrySet()) {
+            if (count1 < 10) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+                count1++;
+            } else {
+                break;
+            }
+        }
     }
 }
